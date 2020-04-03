27a344b977d2fc82bf756642070f0594e2d8decf
ozielusa <ozielusa@users.noreply.github.com>
2020-04-03 13:42:27 -0300

    Add files via upload

--- noip-oziel.sh
+++ noip-oziel.sh
@@ -1,62 +1,4 @@
-
-# Variável da Data Inicial para calcular o tempo de execução do script (VARIÁVEL MELHORADA)
-# opção do comando date: +%T (Time)
-HORAINICIAL='date +%T'
-#
-# Variáveis para validar o ambiente, verificando se o usuário e "root", versão do ubuntu e kernel
-# opções do comando id: -u (user), opções do comando: lsb_release: -r (release), -s (short), 
-# opções do comando uname: -r (kernel release), opções do comando cut: -d (delimiter), -f (fields)
-# opção do caracter: | (piper) Conecta a saída padrão com a entrada padrão de outro comando
-# opção do shell script: acento crase ` ` = Executa comandos numa subshell, retornando o resultado
-# opção do shell script: aspas simples ' ' = Protege uma string completamente (nenhum caractere é especial)
-# opção do shell script: aspas duplas " " = Protege uma string, mas reconhece $, \ e ` como especiais
-USUARIO='id -u'
-UBUNTU='lsb_release -rs'
-KERNEL='uname -r | cut -d'.' -f1,2'
-#
-# Variável do caminho do Log dos Script utilizado nesse curso (VARIÁVEL MELHORADA)
-# opções do comando cut: -d (delimiter), -f (fields)
-# $0 (variável de ambiente do nome do comando)
-#LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
-#
-# Variáveis de configuração do MySQL e liberação de conexão remota para o usuário Root
-USER="ozielusa@hotmail.com"
-PASSWORD="ozi1977noip"
-AGAIN=$PASSWORD
-#
-# Verificando se o usuário e Root, Distribuição e >=18.04 e o Kernel >=4.15 <IF MELHORADO)
-# [ ] = teste de expressão, && = operador lógico AND, == comparação de string, exit 1 = A maioria dos erros comuns na execução
-#clear
-if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "18.04" ] && [ "$KERNEL" == "4.15" ]
-	then
-		echo -e "O usuário e Root, continuando com o script..."
-		echo -e "Distribuição e >=18.04.x, continuando com o script..."
-		echo -e "Kernel e >= 4.15, continuando com o script..."
-		sleep 5
-	else
-		echo -e "Usuário não e Root ($USUARIO) ou Distribuição não e >=18.04.x ($UBUNTU) ou Kernel não e >=4.15 ($KERNEL)"
-		echo -e "Caso você não tenha executado o script com o comando: sudo -i"
-		echo -e "Execute novamente o script para verificar o ambiente."
-		exit 1
-fi
-#clear
-echo -e "Atualizando as listas do Apt, aguarde..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	apt update &>> $LOG
-echo -e "Listas atualizadas com sucesso!!!, continuando com o script..."
-sleep 5
-echo
-#
-echo -e "Atualizando o sistema, aguarde..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	# opção do comando apt: -y (yes)
-	apt -y upgrade &>> $LOG
-echo -e "Sistema atualizado com sucesso!!!, continuando com o script..."
-sleep 5
-echo
-#
-#			 
-echo -e "Acessando o diretorio de destino, aguarde..."
+ echo -e "Acessando o diretorio de destino, aguarde..."
     cd /usr/local/src/
 echo -e "Realizando o download do arquivo de instalacao, aguarde..."
     wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
@@ -71,5 +13,3 @@ echo -e "Abrindo configuracoes de login, aguarde..."
 echo -e "Reinicializando os serviços do NoIP, aguarde..."
 	/usr/local/bin/noip2
 echo -e "Serviço reinicializado com sucesso!!!"
-sleep 5
-echo
